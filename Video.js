982297fe09a808d5d1b5c8ef748daf225d70967b
K4mino <erzhan.kz1923@gmail.com>
2023-01-20 19:05:18 +0600

    uploaded

---
+++ Video.js
@@ -0,0 +1,60 @@
+function Video() {
+	let classes = {
+		item: 'video__wrapper',
+		itemPlaying: 'video__wrapper--playing',
+	};
+
+	let getVideoFrame = function (videoId, source) {
+		let iframe = document.createElement('iframe');
+			iframe.frameBorder = "0";
+			iframe.allow = "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture";
+			iframe.allowFullscreen = "";
+
+		switch (source) {
+			case "vimeo":
+				iframe.src = 'https://player.vimeo.com/video/' + videoId + '?autoplay=1';
+				break;
+			default:
+				iframe.src = 'https://www.youtube.com/embed/' + videoId + '?autoplay=1&rel=0&controls=1&showinfo=0';
+		}
+
+		return iframe;
+	};
+	let play = function (item) {
+		if (item.classList.contains(classes.itemPlaying)) {
+			return false;
+		}
+
+		let videoId = item.getAttribute('data-video-id'),
+			source = item.getAttribute('data-video-source'),
+			iframe = getVideoFrame(videoId, source);
+
+		item.classList.add(classes.itemPlaying);
+		item.appendChild(iframe);
+	};
+	let listenPlayClick = function () {
+		let items = document.getElementsByClassName(classes.item);
+
+		for (let i = 0; i < items.length; i++) {
+			let item = items[i];
+
+			item.addEventListener('click', function () {
+				play(item);
+			});
+		}
+	};
+	let init = function () {
+		listenPlayClick();
+	};
+
+	this.destroy = function (item) {
+		item.classList.remove(classes.itemPlaying);
+		item.removeChild(item.querySelector('iframe'));
+	};
+
+	init();
+}
+
+let btn = document.querySelector('.video__play-btn');
+
+btn.addEventListener('click',Video());
---
+++ app.js
@@ -0,0 +1,13 @@
+let burgerBtn = document.querySelector('.burger-menu');
+let nav = document.querySelector('.header__nav');
+
+burgerBtn.addEventListener('click', function(){
+    nav.classList.toggle('active');
+    burgerBtn.classList.toggle('active');
+});
+
+
+
+
+
+    
